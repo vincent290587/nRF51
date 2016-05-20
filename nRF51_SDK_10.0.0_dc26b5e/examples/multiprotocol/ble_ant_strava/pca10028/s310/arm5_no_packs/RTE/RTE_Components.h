e481f8c79b878768a4e3fb3929bd941c8f715b92
vincent290587 <vincent.golle@gmail.com>
2016-05-20 14:41:35 +0200

    Test nouvelle appli non boguee

---
+++ nRF51_SDK_10.0.0_dc26b5e/examples/multiprotocol/ble_ant_strava/pca10028/s310/arm5_no_packs/RTE/RTE_Components.h
@@ -0,0 +1,20 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'ble_ant_app_hrm_s332_pca10036' 
+ * Target:  'nrf51422_xxaa' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "nrf.h"
+
+
+#endif /* RTE_COMPONENTS_H */
